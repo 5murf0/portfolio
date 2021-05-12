c553f92e5a9c94c425b38f4040ac557e60ead7da
Andrea Maffia <andrea.maffia3@gmail.com>
2021-05-12 16:39:18 +0200

    add about and contact controller actions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get 'pages/portfolio' => 'pages#portfolio'
+  get 'pages/about' => 'pages#about'
+  get 'pages/contact' => 'pages#contact'
 end
