ba8738781a624070a222b4734ca28d254abd3932
Andrea Maffia <andrea.maffia3@gmail.com>
2021-05-24 19:24:13 +0200

    add pages#contact action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get 'pages/portfolio' => 'pages#portfolio'
   get 'pages/about' => 'pages#about'
-
+  get 'pages/contact' => 'pages#contact'
+  
 end
