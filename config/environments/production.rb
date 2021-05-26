f8d6a2e0bafdafddf5d7be710f911fbab3975acd
Andrea Maffia <andrea.maffia3@gmail.com>
2021-05-26 11:27:51 +0200

    change domain in ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'https://andreamaffia.herokuapp.com/',
     :enable_starttls_auto => true
   }
 
