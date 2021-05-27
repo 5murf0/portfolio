373221d7eb7fb57ff4c3199d30b40bb3fac0d613
Andrea Maffia <andrea.maffia3@gmail.com>
2021-05-27 13:13:30 +0200

    remove ActionMailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,18 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://andreamaffia.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',         
-    :authentication => :plain,
-    :user_name      => 'apikey',
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
