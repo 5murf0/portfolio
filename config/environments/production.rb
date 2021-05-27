06d6a98361b935a092241419208278fc4c2124b7
Andrea Maffia <andrea.maffia3@gmail.com>
2021-05-27 11:00:55 +0200

    correct user_name in ActionMailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',         
     :authentication => :plain,
-    :user_name      => ENV['SENGRID_USERNAME'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
